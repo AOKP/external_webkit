CyanogenMod/android_external_webkit

external/webkit: use Cpu Upload path
Use Cpu Upload mode since the Gpu mode is not working
and expected to be slower.

Change-Id: Iacb6de4a4e054e4f6ebb07d8a8209228e78e5ee0

Source/WebCore/platform/graphics/android/TransferQueue.cpp
@@ -479,7 +479,11 @@ void TransferQueue::setTextureUploadType(TextureUploadType type)
     discardQueue();
 
     android::Mutex::Autolock lock(m_transferQueueItemLocks);
+#ifdef QCOM_HARDWARE
+    m_currentUploadType = CpuUpload; // force to cpu upload mode for now until gpu upload mode is fixed
+#else
     m_currentUploadType = type;
+#endif
     XLOGC("Now we set the upload to %s", m_currentUploadType == GpuUpload ? "GpuUpload" : "CpuUpload");
 }
 

AOKP/external_webkit

Source/WebCore/platform/graphics/android/rendering/TransferQueue.cpp
@@ -507,7 +507,11 @@ void TransferQueue::setTextureUploadType(TextureUploadType type)
 
     setPendingDiscard();
 
+#ifdef FORCE_CPU_UPLOAD
+    m_currentUploadType = CpuUpload; // force to cpu upload mode for now until gpu upload mode is fixed
+#else
     m_currentUploadType = type;
+#endif
     ALOGD("Now we set the upload to %s", m_currentUploadType == GpuUpload ? "GpuUpload" : "CpuUpload");
 }
 